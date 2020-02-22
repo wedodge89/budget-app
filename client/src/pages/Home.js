1eca8d9ab4cd660ab3842339b36c1503a1661888
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2020-02-22 09:58:04 -0500

    added Greeting component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Calendar from "../components/Calendar/Calendar";
-import Container from "../components/Container/Container"
+import Container from "../components/Container/Container";
+import Greeting from "../components/Greeting/Greeting"
 
 const Home = props => {
     return(
 
         <Container>
+            <Greeting />
             <Calendar />
         </Container>
     )
