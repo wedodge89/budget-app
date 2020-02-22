52de14020eee65a6fbbd2602cc73ca7ef973ed6d
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2020-02-22 09:56:36 -0500

    added greeting component

---
+++ client/src/components/Greeting/Greeting.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Greeting() {
+      return (
+          <div>
+              <p>
+                  Hello!
+              </p>
+          </div>
+      )
+  };
+
+export default Greeting;
