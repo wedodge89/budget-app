bb41147c05445cbb9fcfce2f14397b449d648c19
Roy Davis <myroy1024@yahoo.com>
2020-02-25 21:12:00 -0500

    bills page working

--- client/src/pages/MyBills.js
+++ client/src/pages/MyBills.js
@@ -5,7 +5,7 @@ import Col from "../components/Col/Col";
 import API from "../utils/API";
 import Card from "../components/BillCard/Card";
 
-class Bills extends Component{
+export default class Bills extends Component{
 state = {
     bills: [],
     name: "",
@@ -27,16 +27,15 @@ handleInputChange = event => {
   };
 
   getMyBills = () => {
-      API.getBills()
+      API.getMyBills()
       .then(res => {
-          let myBills = res.data;
-          for(let i = 0; i < myBills.length; i++) {
-            this.setState({
-                bills: myBills.concat()
-            })
-        }
-        
-        console.log(this.state.bills)
+          let myBills = res.data.bills;
+          this.setState({
+            bills: myBills
+        })
+
+
+       
       })
       .catch(err => console.log(err));
   };
@@ -57,6 +56,26 @@ handleInputChange = event => {
     
   }
 
+  get bills(){
+      const { bills } = this.state
+      console.log(bills);
+      if(Array.isArray(bills)){
+          return bills.map(bill => 
+            <Card 
+            key={bill._id}
+            id={bill._id}
+            name={bill.name}
+            amount={bill.amount}
+            date={bill.date}
+            deleteBill={this.deleteBill}
+            />
+        )
+      }
+
+      return <p>Sorry no bills to show</p>
+
+  }
+
   render(){
       return(
           <Container>
@@ -64,22 +83,12 @@ handleInputChange = event => {
                   <Col size="md-12"><h1>My Bills</h1></Col>
               </Row>
               <Row>
-                  {this.state.bills.map(bill => (
-                  <Col size="md-12" key={bill._id}>
-                      <Card 
-                      id={bill._id}
-                      name={bill.name}
-                      amount={bill.amount}
-                      date={bill.date}
-                      deleteBill={this.deleteBill}
-                      />
+                  <Col size="md-12">
+                      {this.bills}
                   </Col>
-                  ))}
               </Row>
           </Container>
       )
   }
 
 }
-
-export default Bills;
