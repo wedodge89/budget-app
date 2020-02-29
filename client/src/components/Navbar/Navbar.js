4e6dd1abfb0cc8c6f1294bef07efdacf6965deab
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2020-02-29 09:36:51 -0500

    started on navbar styling

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link, Router } from "react-router-dom";
+import "./../../css/Navbar.css"
 
 const Navbar = props => {
     return (
         
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary" id="navHeader">
             <Link className="navbar-brand" to="/">
                 Home
             </Link>
@@ -19,7 +20,7 @@ const Navbar = props => {
                 <ul className="navbar-nav">
                     <li className="nav-item">
                         <Link to="/login" className={window.location.pathname === "/login" ? "nav-link active" : "nav-link"}>
-                            Login
+                            Sign In
                         </Link>
                     </li>
 
@@ -31,26 +32,27 @@ const Navbar = props => {
 
                     <li className="nav-item">
                         <Link to="/budget" className={window.location.pathname === "/budget" ? "nav-link active" : "nav-link"}>
-                            My Budget
+                            My Budgets
                         </Link>
                     </li>
 
                     <li className="nav-item">
                         <Link to="/budgetform" className={window.location.pathname === "/budgetform" ? "nav-link active" : "nav-link"}>
-                            Build Budget
+                            Build Budgets
                         </Link>
                     </li>
+                    <li className="nav-item">
+                        <Link to="/bills" className={window.location.pathname === "/bills" ? "nav-link active" : "nav-link"}>
+                            My Bills
+                        </Link>
+                    </li>
+                    
                     <li className="nav-item">
                         <Link to="/billsform" className={window.location.pathname === "/billsform" ? "nav-link active" : "nav-link"}>
                             Add Bills
                         </Link>
                     </li>
 
-                    <li className="nav-item">
-                        <Link to="/bills" className={window.location.pathname === "/bills" ? "nav-link active" : "nav-link"}>
-                            My Bills
-                        </Link>
-                    </li>
                 </ul>
 
             </div>
