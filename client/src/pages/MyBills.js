87984f0c58b4a66659b38e8d13faa1b6372c4f19
Roy Davis <myroy1024@yahoo.com>
2020-03-03 18:46:01 -0500

    working on heroku

--- client/src/pages/MyBills.js
+++ client/src/pages/MyBills.js
@@ -5,100 +5,99 @@ import Col from "../components/Col/Col";
 import API from "../utils/API";
 import Card from "../components/BillCard/Card";
 import Calendar from "../components/Calendar/Calendar";
-import {withRouter} from "react-router-dom";
+import { withRouter } from "react-router-dom";
 
 
- class Bills extends Component{
-state = {
+class Bills extends Component {
+  state = {
     bills: [],
     name: "",
     amount: "",
     date: "",
     paid: ""
-};
+  };
 
-componentDidMount() {
-this.getMyBills();
-}
+  componentDidMount() {
+    this.getMyBills();
+  }
 
-handleInputChange = event => {
+  handleInputChange = event => {
     const { name, value } = event.target;
     this.setState({
       [name]: value
     });
-    
-    
+
+
   };
 
   getMyBills = () => {
-      API.getMyBills()
+    API.getMyBills()
       .then(res => {
-          let myBills = res.data.bills;
-          this.setState({
-            bills: myBills
+        let myBills = res.data.bills;
+        this.setState({
+          bills: myBills
         })
 
 
-       
+
       })
       .catch(err => console.log(err));
   };
 
   deleteBill = (id) => {
-      API.deleteMyBill(id)
+    API.deleteMyBill(id)
       .then(res => {
-      this.getMyBills()
-    })
-    .catch(err => console.log(err));
+        this.getMyBills()
+      })
+      .catch(err => console.log(err));
     console.log("click working")
-    
+
   }
   updateBill = (id) => {
     console.log(id)
     this.props.updateBillId(id);
     this.props.history.push("/updatebills")
   }
-  
 
-    get bills(){
-      const { bills } = this.state
-      console.log(bills);
-      if(Array.isArray(bills)){
-          return bills.map(bill => 
-            <Card 
-            key={bill._id}
-            _id={bill._id}
-            name={bill.name}
-            amount={bill.amount}
-            date={bill.date}
-            paid={bill.paid}
-            deleteBill={this.deleteBill}
-            updateBill={this.updateBill}
-            />
-        )
-      }
-      
 
-      return <h4>Sorry no bills to show</h4>
 
+  get bills() {
+    const { bills } = this.state
+    console.log(bills);
+    if (Array.isArray(bills)) {
+      return bills.map(bill =>
+        <Card
+          key={bill._id}
+          _id={bill._id}
+          name={bill.name}
+          amount={bill.amount}
+          date={bill.date}
+          paid={bill.paid}
+          deleteBill={this.deleteBill}
+          updateBill={this.updateBill}
+        />
+      )
+    }
+    return <h4>Sorry no bills to show</h4>
   }
 
-  render(){
-      return(
-          <Container>
-              <Row>
-                  <Col size="md-12"><h1>My Bills</h1></Col>
-              </Row>
-              <Row>
-                  <Col size="md-6" id="billCardCol">
-                      {this.bills}
-                  </Col>
-                  <Col size="md-6">
-                    <Calendar></Calendar>
-                  </Col>
-              </Row>
-          </Container>
-      )
+
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col size="md-12"><h1>My Bills</h1></Col>
+        </Row>
+        <Row>
+          <Col size="md-6" id="billCardCol">
+            {this.bills}
+          </Col>
+          <Col size="md-6">
+            <Calendar></Calendar>
+          </Col>
+        </Row>
+      </Container>
+    )
   }
 
 }
