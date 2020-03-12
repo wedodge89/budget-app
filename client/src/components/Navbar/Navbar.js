af4514a79c03b895be4d6b6c432e109a90855171
Roy Davis <myroy1024@yahoo.com>
2020-03-11 20:55:26 -0400

    rounded borders

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -13,15 +13,15 @@ class Navbar extends Component {
             </Link>
             <button className="navbar-toggler" type="button"
                 data-toggle="collapse"
-                data-target="#navbarToggler"
-                aria-controls="navbarToggler" aria-expanded="false"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse justify-content-end" id="navbarToggler">
+            <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <Link to="/login" className={window.location.pathname === "/login" ? "nav-link active" : "nav-link"}>
+                        <Link to="/login" className={window.location.pathname === "/login"  ? "nav-link active" : "nav-link"}>
                             Sign In
                         </Link>
                     </li>
